bd193b195b5a994d5cdbfd3874a60e623a160c0f
MamushkaDead <gasparkappou@gmail.com>
2023-03-26 23:37:00 -0300

    copitos de nieve god

--- kappou/Copito/Program.cs
+++ kappou/Copito/Program.cs
@@ -1,20 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Copito
+namespace Nieve
 {
     class Copo
     {
-        public int x, y, limit;
+        public int x, y;
 
         public Copo(int x, int y)
         {
             this.x = x;
             this.y = y;
-            this.limit = 10;
         }
 
         public void mostrar()
@@ -23,65 +23,76 @@ namespace Copito
             Console.Write("*");
         }
 
-        public void bajar(List<Copo> list)
+        public void bajar(int altura)
         {
-            if (y < limit)
+            if (y < altura)
             {
                 Console.SetCursorPosition(x, y);
                 Console.Write(" ");
-
-                if (y + 1 < limit && list.FirstOrDefault(c => c.x == x && c.y == y + 1) == null)
-                {
-                    // La posición debajo del copo está vacía, por lo que simplemente se mueve hacia abajo
-                    y++;
-                }
-                else
-                {
-                    // La posición debajo del copo está ocupada, por lo que se elimina el copo actual
-                    // y se agrega un nuevo copo en la posición superior de la misma columna
-                    list.Remove(this);
-                    list.Add(new Copo(x, y));
-                }
-
+                y++;
                 Console.SetCursorPosition(x, y);
                 Console.Write("*");
-            }
-            else
-            {
-                // Si se alcanza el límite superior, se elimina el copo actual
-                list.Remove(this);
             }
         }
     }
     internal class Program
     {
-
         static void Main(string[] args)
         {
-            List<Copo> Copos = new List<Copo>();
+            int ancho = 5;
+            int altura = 5;
+            int cantPart = 0;
+            List<Copo> list = new List<Copo>();
             Random rand = new Random();
 
             DateTime hora = DateTime.Now;
-            DateTime horaActual = DateTime.Now;
+            //DateTime horaActual = DateTime.Now;
             TimeSpan tiempoTrasncurrido;
 
+            bool bajar;
             Console.CursorVisible = false;
             while (true)
             {
-
-                horaActual = DateTime.Now;
+                DateTime horaActual = DateTime.Now;
                 tiempoTrasncurrido = horaActual - hora;
-                if (tiempoTrasncurrido.Milliseconds == 300)
+                if (tiempoTrasncurrido.Milliseconds >= 500)
                 {
-                    Copos.Add(new Copo(rand.Next(6), 0));
+                    list.Add(new Copo(rand.Next(ancho), 0));
                     hora = DateTime.Now;
-
-                    for (int i = Copos.Count - 1; i >= 0; i--)
+                    foreach (Copo copoLista in list)
                     {
-                        Copo copoLista = Copos[i];
-                        copoLista.bajar(Copos);
-                        copoLista.mostrar();
+                        if(copoLista.y == altura)
+                        {
+                            cantPart++;
+                        }
+                        bajar = true;
+                        foreach (Copo temp in list)
+                        {
+                            if ((temp.x == copoLista.x) && (temp.y == copoLista.y + 1))
+                            {
+                                bajar = false;
+                                break;
+                            }  
+                        }
+                        if (bajar)
+                        {
+                            copoLista.bajar(altura);
+                            copoLista.mostrar();
+                        }
                     }
+                    if (cantPart == ancho)
+                    {
+                        for(int i = list.Count-1; i >= 0; i--)
+                        {
+                            if (list[i].y == altura)
+                            {
+                                list.RemoveAt(i);
+                            }
+                        }
+                        Console.SetCursorPosition(0, altura);
+                        Console.Write("                                      ");
+                    }
+                    cantPart = 0;
                 }
             }
         }
