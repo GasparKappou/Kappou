d1e4689ad29a738e254c6dbc71f2d51fb8b7f124
Kappou <106691185+MamushkaDead@users.noreply.github.com>
2023-03-30 15:19:08 -0300

    Jaj

---
+++ kappou/ejercicio/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Persona
+{
+    public string name;
+    public string dni;
+
+    //Constructor por defaul - Mismo nombre que la clase
+    public Persona()
+    {
+    }
+
+
+    public Persona(string name, string dni)
+    {
+        this.name = name;
+        this.dni = dni;
+    }
+    //Mas de un constructor = sobrecarga de constructores
+
+    //Metodo para mostrar los datos del objeto
+    public string mostrarTodo()
+    {
+        return name + ", " + dni;
+    }
+}
+
+internal class Program
+{
+    static List<Persona> personas = new List<Persona>();
+
+    static Persona buscarDni(string dni)
+    {
+        for (int i = 0; i < personas.Count; i++)
+        {
+            if (personas[i].dni == dni)
+                return personas[i];
+        }
+        return null;
+    }
+
+    static void Main(string[] args)
+    {
+        Persona persona1;
+        /*
+        persona1 == AB ("pablo", 10)
+        list == [0] => FF("sergio", 14)
+                [1] => AB("pablo", 10)
+        
+
+        
+        for (int i = 0; i < personas.Count; i++)
+        {
+            Console.WriteLine(personas[i].dni);
+        }
+        Console.ReadLine();
+        */
+        personas.Add(new Persona("Sergio", "aj1234"));
+        personas.Add(new Persona("Pablo", "aj2345"));
+        personas.Add(new Persona("Florencia", "uj3456"));
+
+        string dni;
+
+        Console.Write("Ingrese un dni para buscar: ");
+        dni = Console.ReadLine();
+        Persona personaBuscada = buscarDni(dni);
+        if (personaBuscada != null)
+        {
+            Console.WriteLine("Se encontro");
+            Console.WriteLine(personaBuscada.name);
+            Console.ReadKey();
+        }
+        else
+        {
+            Console.WriteLine("No se encontro");
+            Console.ReadKey();
+        }
+
+        Console.Write("Ingrese un dni a buscar para luego borrar: ");
+        dni = Console.ReadLine();
+        persona1 = buscarDni(dni);
+        if (persona1 != null)
+        {
+            personas.Remove(persona1);
+        }
+
+        for (int i = 0; i < personas.Count; i++)
+        {
+            Console.WriteLine(personas[i].mostrarTodo());
+        }
+        Console.ReadKey();
+        
+    }
+}
--- kappou/ejercicio/Properties/AssemblyInfo.cs
+++ kappou/ejercicio/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // La información general de un ensamblado se controla mediante el siguiente 
 // conjunto de atributos. Cambie estos valores de atributo para modificar la información
 // asociada a un ensamblado.
-[assembly: AssemblyTitle("Juego AI")]
+[assembly: AssemblyTitle("ejercicio")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Juego AI")]
+[assembly: AssemblyProduct("ejercicio")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
-[assembly: Guid("8da493d4-8563-4183-90bc-4b94adc34704")]
+[assembly: Guid("8e111e6c-f521-4c6e-bc9c-d4e186524c73")]
 
 // La información de versión de un ensamblado consta de los cuatro valores siguientes:
 //
