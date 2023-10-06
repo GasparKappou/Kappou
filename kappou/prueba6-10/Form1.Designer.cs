502b5cc39ac167469a63f68a62281e1d025c6a70
mamushkadead <gasparkappou@gmail.com>
2023-10-06 14:49:34 -0300

    prueba 6/10

---
+++ kappou/prueba6-10/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace prueba6_10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Ordenar = new System.Windows.Forms.Button();
+            this.nombresA = new System.Windows.Forms.ListBox();
+            this.nombresB = new System.Windows.Forms.ListBox();
+            this.Last_and_first = new System.Windows.Forms.Button();
+            this.Borrar_menos_5 = new System.Windows.Forms.Button();
+            this.Intercambiar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Ordenar
+            // 
+            this.Ordenar.Location = new System.Drawing.Point(12, 12);
+            this.Ordenar.Name = "Ordenar";
+            this.Ordenar.Size = new System.Drawing.Size(75, 23);
+            this.Ordenar.TabIndex = 0;
+            this.Ordenar.Text = "Ordenar";
+            this.Ordenar.UseVisualStyleBackColor = true;
+            this.Ordenar.Click += new System.EventHandler(this.Ordenar_Click);
+            // 
+            // nombresA
+            // 
+            this.nombresA.FormattingEnabled = true;
+            this.nombresA.Location = new System.Drawing.Point(12, 41);
+            this.nombresA.Name = "nombresA";
+            this.nombresA.Size = new System.Drawing.Size(387, 368);
+            this.nombresA.TabIndex = 4;
+            this.nombresA.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // nombresB
+            // 
+            this.nombresB.FormattingEnabled = true;
+            this.nombresB.Location = new System.Drawing.Point(401, 41);
+            this.nombresB.Name = "nombresB";
+            this.nombresB.Size = new System.Drawing.Size(387, 368);
+            this.nombresB.TabIndex = 5;
+            // 
+            // Last_and_first
+            // 
+            this.Last_and_first.Location = new System.Drawing.Point(713, 415);
+            this.Last_and_first.Name = "Last_and_first";
+            this.Last_and_first.Size = new System.Drawing.Size(75, 23);
+            this.Last_and_first.TabIndex = 3;
+            this.Last_and_first.Text = "Last and first";
+            this.Last_and_first.UseVisualStyleBackColor = true;
+            this.Last_and_first.Click += new System.EventHandler(this.Last_and_first_Click);
+            // 
+            // Borrar_menos_5
+            // 
+            this.Borrar_menos_5.Location = new System.Drawing.Point(12, 415);
+            this.Borrar_menos_5.Name = "Borrar_menos_5";
+            this.Borrar_menos_5.Size = new System.Drawing.Size(75, 23);
+            this.Borrar_menos_5.TabIndex = 6;
+            this.Borrar_menos_5.Text = "Borrar <5";
+            this.Borrar_menos_5.UseVisualStyleBackColor = true;
+            this.Borrar_menos_5.Click += new System.EventHandler(this.Borrar_menos_5_Click);
+            // 
+            // Intercambiar
+            // 
+            this.Intercambiar.Location = new System.Drawing.Point(713, 12);
+            this.Intercambiar.Name = "Intercambiar";
+            this.Intercambiar.Size = new System.Drawing.Size(75, 23);
+            this.Intercambiar.TabIndex = 7;
+            this.Intercambiar.Text = "Intercambiar";
+            this.Intercambiar.UseVisualStyleBackColor = true;
+            this.Intercambiar.Click += new System.EventHandler(this.Intercambiar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Intercambiar);
+            this.Controls.Add(this.Borrar_menos_5);
+            this.Controls.Add(this.nombresB);
+            this.Controls.Add(this.nombresA);
+            this.Controls.Add(this.Last_and_first);
+            this.Controls.Add(this.Ordenar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Ordenar;
+        private System.Windows.Forms.ListBox nombresA;
+        private System.Windows.Forms.ListBox nombresB;
+        private System.Windows.Forms.Button Last_and_first;
+        private System.Windows.Forms.Button Borrar_menos_5;
+        private System.Windows.Forms.Button Intercambiar;
+    }
+}
+
---
+++ kappou/prueba6-10/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prueba6_10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            nombresA.Items.Add("Sofia");
+            nombresA.Items.Add("Claudia");
+            nombresA.Items.Add("Tamara");
+            nombresA.Items.Add("Angie");
+            nombresA.Items.Add("Azul");
+            nombresA.Items.Add("Gaspar");
+            nombresA.Items.Add("Luis");
+            nombresA.Items.Add("Tobias");
+            nombresA.Items.Add("Felipe");
+            nombresA.Items.Add("Magda");
+
+            nombresB.Items.Add("Jordana");
+            nombresB.Items.Add("Ulises");
+            nombresB.Items.Add("Alejandro");
+            nombresB.Items.Add("Jeronimo");
+            nombresB.Items.Add("Gustavo");
+            nombresB.Items.Add("Macarena");
+            nombresB.Items.Add("Candela");
+            nombresB.Items.Add("Malena");
+            nombresB.Items.Add("Eneas");
+            nombresB.Items.Add("Matias");
+        }
+        private void Ordenar_Click(object sender, EventArgs e)
+        {
+            nombresA.Sorted = true;
+            nombresB.Sorted = true;
+        }
+        private void Borrar_menos_5_Click(object sender, EventArgs e)
+        {
+            for (int i = nombresA.Items.Count - 1; i >= 0; i--)
+                if (nombresA.Items[i].ToString().Count() <= 5)
+                    nombresA.Items.RemoveAt(i);
+            for (int i = nombresB.Items.Count - 1; i >= 0; i--)
+                if (nombresB.Items[i].ToString().Count() <= 5)
+                    nombresB.Items.RemoveAt(i);
+        }
+        private void Intercambiar_Click(object sender, EventArgs e)
+        {
+            int indiceA = 0;
+            int indiceB = 0;
+            ListBox nomC = new ListBox();
+            nomC.Items.Add("itemEjemplo");
+            for (int i = indiceA; i < nombresA.Items.Count; i++)
+            {
+                indiceA = i;
+                if (nombresA.Items[i].ToString().Count() <= 5)
+                    for (int j = indiceB; j < nombresB.Items.Count; j++)
+                    {
+                        indiceB = j;
+                        if (nombresB.Items[j].ToString().Count() <= 5)
+                        {
+                            nomC.Items[0] = nombresA.Items[i].ToString();
+                            nombresA.Items[i] = nombresB.Items[j];
+                            nombresB.Items[j] = nomC.Items[0];
+                        }
+                    }
+            }
+        }
+
+        private void Last_and_first_Click(object sender, EventArgs e)
+        {
+            ListBox nombresC = new ListBox();
+            nombresC.Items.Add(nombresA.Items[0].ToString());
+            nombresC.Items.Add(nombresA.Items[nombresA.Items.Count - 1].ToString());
+            nombresC.Items.Add(nombresB.Items[0].ToString());
+            nombresC.Items.Add(nombresB.Items[nombresB.Items.Count - 1].ToString());
+
+            nombresA.Items[0] = nombresC.Items[2];
+            nombresA.Items[nombresA.Items.Count - 1] = nombresC.Items[3];
+            nombresB.Items[0] = nombresC.Items[0];
+            nombresB.Items[nombresB.Items.Count - 1] = nombresC.Items[1];
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
