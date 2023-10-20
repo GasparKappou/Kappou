bb6b83ed54467ca25872f91bfd7a77b366b6b808
mamushkadead <gasparkappou@gmail.com>
2023-10-20 14:38:50 -0300

    juego

---
+++ kappou/Project1/Character.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    class Character
+    {
+        private Texture2D texture;
+        private Vector2 position;
+        private Vector2 velocity;
+        private float rotation;
+
+        public Character(Texture2D texture, Vector2 position)
+        {
+            this.texture = texture;
+            this.position = position;
+            this.velocity = Vector2.Zero;
+            this.rotation = 0f;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            // Implement bird movement and physics logic here
+            // Update position, velocity, rotation, etc.
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, position, null, Color.White, rotation, Vector2.Zero, 1f, SpriteEffects.None, 0f);
+        }
+    }
+}
---
+++ kappou/Project1/Game1.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Net.Mime;
+using System.Threading;
+
+namespace Project1
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        static Texture2D star;
+        Character character;
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = false;
+            _graphics.PreferredBackBufferHeight = 720;
+            _graphics.PreferredBackBufferWidth = 1280;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            star = Content.Load<Texture2D>("starChar");
+            character = new Character(star, new Vector2(100, 100));
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            KeyboardState keyboardState = Keyboard.GetState();
+
+            if (keyboardState.IsKeyDown(Keys.Space))
+            {
+                // Implement jump logic here
+            }
+            // Update input, game objects, collision detection, etc.
+            character.Update(gameTime);
+
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            _spriteBatch.Begin();
+
+            // Draw game objects
+            character.Draw(_spriteBatch);
+
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+
+            base.Draw(gameTime);
+        }
+    }
+}
