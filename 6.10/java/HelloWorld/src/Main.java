96839a8402180a969900caf0364ebce17dddce9f
Kappou <gasparkappou@gmail.com>
2024-03-19 02:39:28 -0300

    dlroWolleh

---
+++ 6.10/java/HelloWorld/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        helloWorld("print");
+    }
+    public static void helloWorld(String texto){
+        System.out.println(texto);
+    }
+}
