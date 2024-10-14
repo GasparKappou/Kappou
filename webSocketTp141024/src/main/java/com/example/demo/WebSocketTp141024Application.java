a8401fa1eb0261c696ef4c03641a9cdea1e90a7b
Kappou <106691185+GasparKappou@users.noreply.github.com>
2024-10-14 20:55:28 -0300

    web socket

---
+++ webSocketTp141024/src/main/java/com/example/demo/WebSocketTp141024Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSocketTp141024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSocketTp141024Application.class, args);
+	}
+
+}
